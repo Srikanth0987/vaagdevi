43a9c4981385fbfddaa1f0edbe9b2952e11fe6fa
Srikanth0987 <33112605+Srikanth0987@users.noreply.github.com>
2020-02-23 12:33:07 +0530

    hello.java file added to githup repo
    hello.java file added to GitHub repo

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.applet.*;
+import java.awt.*;
+		public class  	Hello extends Applet
+{
+public void paint(Graphics g)
+	{
+		  g.drawString("welcome",10,100);
+	}
+}
+
